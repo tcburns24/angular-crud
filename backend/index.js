dc54eca8cbff2edcaf3ef889f63ac2764ff9563b
Thomas Burns <tcburns24@gmail.com>
2025-06-17 11:19:26 -0400

    Changes createConnection to createPool

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // database configuration
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 14,
+  queueLimit: 0,
 });
 
 db.connect((err) => {
